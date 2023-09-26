12f4ade6dac7cd7dd13dc04f33219270cd5bc4fc
M.S.T.O.P <mstop4@gmail.com>
2023-09-25 22:50:07 -0400

    trsting rotation

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,8 @@ public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,15 +27,29 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
         Vector2 inputVector = movement.ReadValue<Vector2>();
+        float newX = Mathf.Clamp(transform.localPosition.x + inputVector.x * moveSpeed * Time.deltaTime, -xRange, xRange);
+        float newY = Mathf.Clamp(transform.localPosition.y + inputVector.y * moveSpeed * Time.deltaTime, -yRange, yRange);
+
         transform.localPosition = new Vector3(
-            transform.localPosition.x + inputVector.x * moveSpeed * Time.deltaTime,
-            transform.localPosition.y + inputVector.y * moveSpeed * Time.deltaTime,
+            newX,
+            newY,
             transform.localPosition.z
         );
 
         // float horizontalThrow = Input.GetAxis("Horizontal");
         // float verticalThrow = Input.GetAxis("Vertical");
         // Debug.Log("H: " + horizontalThrow + "\nV: " + verticalThrow);
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
     }
 }
